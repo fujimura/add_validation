a8a6f554dc4eec1f9d1ec955b32949ffc042b6c2
Fujimura Daisuke <me@fujimuradaisuke.com>
2012-12-26 09:48:29 +0900

    More example-ish specs

--- spec/active_model/add_validation_spec.rb
+++ spec/active_model/add_validation_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  describe 'at creation' do
+  describe 'with preparation' do
     let(:user) { User.new name: "Alice" }
     before do
       user.prepare_to_create_primary_address
     end
 
-    context 'with no addresses' do
+    it 'should be able to set primary address' do
+      expect do
+        user.primary_address = nil
+      end.to_not raise_error NoMethodError
+    end
+
+    context 'when no address was given' do
       it 'should be invalid' do
         user.primary_address = nil
         user.valid?
@@ -15,26 +21,31 @@ describe User do
       end
     end
 
-    context 'with one address' do
-      before do
+    context 'when an address was given' do
+      it 'should be valid' do
         user.primary_address = Address.new tel: '090-1234-5678'
         user
-      end
-
-      it 'should be valid' do
         user.should be_valid
       end
     end
 
     describe 'created address' do
-      before do
+      it 'should be primary address' do
         user.primary_address = Address.new tel: '090-1234-5678'
         user.save_with_primary_address
-      end
-
-      it 'should be primary address' do
         user.addresses.first.should be_primary
       end
+    end
+  end
+
+  describe 'with no preparation' do
+    let(:user) { User.new name: "Alice" }
+    # do no preparation
+
+    it 'should not be able to set primary address' do
+      expect do
+        user.primary_address = nil
+      end.to raise_error NoMethodError
     end
   end
 end
